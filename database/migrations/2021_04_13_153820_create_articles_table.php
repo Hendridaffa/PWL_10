292055e6c88c864485e8ba0d2fe96b884a298d14
Hendridaffa <hendridaffa23@gmail.com>
2021-04-13 22:43:27 +0700

    Prak 1 - Mengubah fungsi up()

--- database/migrations/2021_04_13_153820_create_articles_table.php
+++ database/migrations/2021_04_13_153820_create_articles_table.php
@@ -15,6 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('contenct');
+            $table->string('featured_image');
             $table->timestamps();
         });
     }
